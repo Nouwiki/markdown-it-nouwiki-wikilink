6d6489737d432932e4db75c5de3434a57f652a9b
Brynjar Harðarson <brynjar@gmail.com>
2015-10-15 18:31:53 +0000

    fixed bug that caused all links in a line to have the href of the first link item

--- test/test.js
+++ test/test.js
@@ -14,14 +14,26 @@ describe('markdown-it-wikilink', function () {
     s = '[text](page)';
     target = '<p><a href="page">text</a></p>\n';
     expect(md.render(s)).to.equal(target);
+
+    s = '[text](<page a>)';
+    target = '<p><a href="page%20a">text</a></p>\n';
+    expect(md.render(s)).to.equal(target);
+
+    s = '[text](<page a>), [text](<page b>)';
+    target = '<p><a href="page%20a">text</a>, <a href="page%20b">text</a></p>\n';
+    expect(md.render(s)).to.equal(target);
   });
 
-  it('should add alter links that are empty', function() {
+  it('should alter links that are empty', function() {
     var md = require('markdown-it')().use(require('../'));
     var s, target;
 
     s = '[page]()';
     target = '<p><a href="page">page</a></p>\n';
+    expect(md.render(s)).to.equal(target);
+
+    s = '[page a](), [page b]()';
+    target = '<p><a href="page%20a">page a</a>, <a href="page%20b">page b</a></p>\n';
     expect(md.render(s)).to.equal(target);
   });
 
