7cedd25d47b4981e0071c1a02a7bc5facef290f0
Brynjar Harðarson <brynjar@gmail.com>
2016-02-02 17:43:30 +0000

    support for head and tail definition (add to front and back of all wikilinks)

--- dist/markdown-it-wikilink.js
+++ dist/markdown-it-wikilink.js
@@ -5,13 +5,20 @@
 module.exports = function wikilink_plugin(md, scheme) {
   var oldLinkOpenOverride = md.renderer.rules.link_open;
 
+  var head = "";
+  var tail = "";
+  if (scheme != undefined) {
+    head = scheme.head || head;
+    tail = scheme.tail || tail;
+  }
+
   scheme = scheme || 'http://';
 
   md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
     var hrefIndex = tokens[idx].attrIndex('href');
 
-    if (hrefIndex >= 0 && tokens[idx].attrs[hrefIndex][1] == "") {
-      tokens[idx].attrs[hrefIndex][1] = encodeURI(tokens[idx+1].content);
+    if (hrefIndex >= 0 && tokens[idx].attrs[hrefIndex][1] == "") { // if markdown link doesn't contain a url use the text as url (wikilink)
+      tokens[idx].attrs[hrefIndex][1] = head+encodeURI(tokens[idx+1].content)+tail;
     }
 
     if (oldLinkOpenOverride) {
--- test/test.js
+++ test/test.js
@@ -37,4 +37,17 @@ describe('markdown-it-wikilink', function () {
     expect(md.render(s)).to.equal(target);
   });
 
+  it('should add /wiki/ to the front of the link and .html to the back', function() {
+    var md = require('markdown-it')().use(require('../'), {"head": "/wiki/", "tail": ".html"});
+    var s, target;
+
+    s = '[page]()';
+    target = '<p><a href="/wiki/page.html">page</a></p>\n';
+    expect(md.render(s)).to.equal(target);
+
+    s = '[page a](), [page b]()';
+    target = '<p><a href="/wiki/page%20a.html">page a</a>, <a href="/wiki/page%20b.html">page b</a></p>\n';
+    expect(md.render(s)).to.equal(target);
+  });
+
 });
